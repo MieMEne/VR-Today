35b94d73c0ba4b19b81100781865bb4155d2b39d
Mie Enevoldsen <145563042+MieMEne@users.noreply.github.com>
2026-01-13 12:27:33 +0100

    game

---
+++ test_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,73 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void ModelCustomizer::.ctor()
+extern void ModelCustomizer__ctor_m6A9584570F77D03DDA177F66B16044DCDEAC697D (void);
+// 0x00000002 System.Void MouseLook::Start()
+extern void MouseLook_Start_m08FD16A47A96624540CD1B8EBA5FFAA1FD75E686 (void);
+// 0x00000003 System.Void MouseLook::Update()
+extern void MouseLook_Update_m18C0A114A78C19B888A9F74CA1FE5A837DF9E42A (void);
+// 0x00000004 System.Void MouseLook::.ctor()
+extern void MouseLook__ctor_m4C8A79A3CB8D46D3E22E3AB1F645D41ED589312B (void);
+// 0x00000005 System.Void PlayerMovement::Update()
+extern void PlayerMovement_Update_m5BB6CE35AF68EE00CFEB4BA5EBA17E10667551D3 (void);
+// 0x00000006 System.Void PlayerMovement::.ctor()
+extern void PlayerMovement__ctor_mB37559C5B0638161878D20E00B7C672FC38BBBAA (void);
+// 0x00000007 System.Void Unity.Template.VR.XRPlatformControllerSetup::Start()
+extern void XRPlatformControllerSetup_Start_m1F22FCA29DFD83DC0E343C3F391D04A7C52085BF (void);
+// 0x00000008 System.Void Unity.Template.VR.XRPlatformControllerSetup::.ctor()
+extern void XRPlatformControllerSetup__ctor_mF9A3998AF90962CF8F35BAF2221558BDF5F6596E (void);
+static Il2CppMethodPointer s_methodPointers[8] = 
+{
+	ModelCustomizer__ctor_m6A9584570F77D03DDA177F66B16044DCDEAC697D,
+	MouseLook_Start_m08FD16A47A96624540CD1B8EBA5FFAA1FD75E686,
+	MouseLook_Update_m18C0A114A78C19B888A9F74CA1FE5A837DF9E42A,
+	MouseLook__ctor_m4C8A79A3CB8D46D3E22E3AB1F645D41ED589312B,
+	PlayerMovement_Update_m5BB6CE35AF68EE00CFEB4BA5EBA17E10667551D3,
+	PlayerMovement__ctor_mB37559C5B0638161878D20E00B7C672FC38BBBAA,
+	XRPlatformControllerSetup_Start_m1F22FCA29DFD83DC0E343C3F391D04A7C52085BF,
+	XRPlatformControllerSetup__ctor_mF9A3998AF90962CF8F35BAF2221558BDF5F6596E,
+};
+static const int32_t s_InvokerIndices[8] = 
+{
+	4376,
+	4376,
+	4376,
+	4376,
+	4376,
+	4376,
+	4376,
+	4376,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	8,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
